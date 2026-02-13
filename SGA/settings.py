183272c251451ba95c8b12ec210ae783265dbd8b
RuiTunga20 <ruitunga20@gmail.com>
2026-02-13 19:03:51 +0100

    setting

--- SGA/settings.py
+++ SGA/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-r#f7)g%=rdp8ld331qv3q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '192.168.0.213,localhost,127.0.0.1,0.0.0.0,192.168.1.191,192.168.1.192,sga-c3f2.onrender.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '192.168.0.213,localhost,127.0.0.1,0.0.0.0,192.168.1.191,192.168.1.192,sigap.onrender.com').split(',')
 
 # CSRF Trusted Origins (para Docker/produção)
 # CSRF Trusted Origins (para Docker/produção)
@@ -34,7 +34,7 @@ CSRF_TRUSTED_ORIGINS_DEFAULT = [
     'http://192.168.1.191:8000',
     'http://192.168.1.192',
     'http://192.168.1.192:8000',
-    'https://sga-c3f2.onrender.com'
+    'https://sigap.onrender.com'
 ]
 CSRF_TRUSTED_ORIGINS = list(filter(None, set(CSRF_TRUSTED_ORIGINS_ENV + CSRF_TRUSTED_ORIGINS_DEFAULT)))
 
